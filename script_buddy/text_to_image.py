78f7defa60cb44b65a90781f43a28c7c5609cf67
Charles Pierse <charlespierse@gmail.com>
2020-03-31 16:38:45 +0100

    Add image texture file

--- script_buddy/text_to_image.py
+++ script_buddy/text_to_image.py
@@ -2,13 +2,20 @@ import PIL
 from PIL import Image, ImageDraw, ImageFont
 from utils import load_model, generate
 
-img = Image.new('RGB', (500, 650), color = 'white')
+model, tokenizer = load_model()
+font = ImageFont.truetype("Courier New.ttf", 65)
+string = generate(model,tokenizer,input_text="       He pulls his loaded gun out", max_length=1000)[0]
+imsize = font.getsize(string) 
+img = Image.open('paper.png')
 
 d = ImageDraw.Draw(img)
-font = ImageFont.truetype("Courier New.ttf", 12)
-model, tokenizer = load_model()
 
-string = generate(model,tokenizer,max_length= 400)[0]
-d.text((0,0), string, fill=(0,0,0), font= font)
 
-img.save('script_output.png', dpi =(460,460))
+d.text((5,5), string,(0,0,0),font= font,)
+
+img.save('script_output.png',quality =100, optimize= True)
+
+
+# def encode(sample : string) -> PIL.Image():
+#     img = Image.new()
+
